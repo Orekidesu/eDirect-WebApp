c8cc755203d947d1972aa96e09010c486991a355
Orekidesu <cmcodog10@gmail.com>
2024-02-19 04:10:44 +0800

    optimize hover signout

--- src/components/Navigation/NavigationBar.vue
+++ src/components/Navigation/NavigationBar.vue
@@ -39,10 +39,12 @@
             </div>
             <ul class="py-2" aria-labelledby="user-menu-button">
               <li>
-                <button @click="handleSignOut"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                  Sign out
-                </button>
+                <div class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+                  <button @click="handleSignOut"
+                    class="text-sm text-gray-700 dark:text-gray-200 dark:hover:text-white w-full text-left">
+                    Sign out
+                  </button>
+                </div>
               </li>
             </ul>
           </div>
