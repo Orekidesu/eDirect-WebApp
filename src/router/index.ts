9231268f1a75b2315496bd122cab90948ac6f7db
Orekidesu <cmcodog10@gmail.com>
2024-02-20 14:24:14 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import{ createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Login from '@/views/LoginView.vue';
 import Dashboard from '@/views/DashboardView.vue';
 import About from '@/views/AboutView.vue';
@@ -7,43 +7,42 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../firebase/init.js';
 
 const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    name: 'login',
-    component: Login
-  },
- {
-  path: '/dashboard',
-  name: 'dashboard',
-  component: Dashboard,
-  beforeEnter: (to, from, next) => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in, let them access the route
-        next();
-      } else {
-        // No user is signed in, redirect to login
-        next({ name: 'login' });
-      }
-    });
-  }
- },
-  {
-    path: '/about',
-    name: 'about',
-    component: About
-  },
-  {
-  path: '/signup',
-  name: 'signup',
-  component: Signup
-  },
- 
+	{
+		path: '/',
+		name: 'login',
+		component: Login
+	},
+	{
+		path: '/dashboard',
+		name: 'dashboard',
+		component: Dashboard,
+		beforeEnter: (_to, _from, next) => {
+			onAuthStateChanged(auth, (user) => {
+				if (user) {
+					// User is signed in, let them access the route
+					next();
+				} else {
+					// No user is signed in, redirect to login
+					next({ name: 'login' });
+				}
+			});
+		}
+	},
+	{
+		path: '/about',
+		name: 'about',
+		component: About
+	},
+	{
+		path: '/signup',
+		name: 'signup',
+		component: Signup
+	}
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes
 });
 
 export default router;
