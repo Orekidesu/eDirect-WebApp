0557cd0bb01979c978bc8e88ebc5a5fd51195a60
Orekidesu <cmcodog10@gmail.com>
2024-02-21 16:49:53 +0800

    Update NavigationBar.vue

--- src/components/Navigation/NavigationBar.vue
+++ src/components/Navigation/NavigationBar.vue
@@ -1,92 +1,145 @@
-
 <template>
-  <nav class="bg-white border-gray-200 dark:bg-gray-900">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+  <nav class="border-b-[#070F2C] dark:bg-[#1B1A56] text-gray-200 dark:text-white h-16   ">
+    <!-- <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"> -->
+    <div
+      :class="route.name !== 'login' && route.name !== 'signup' ? ' max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4' : ' w-screen max-w-screen-xl flex flex-wrap items-center justify-end mx-auto p-4'">
+      <a v-if="route.name !== 'login'" href="" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <!-- <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">eDirect</span> -->
+
+        <span
+          :class="route.name !== 'login' && route.name !== 'signup' ? 'self-center text-2xl font-semibold whitespace-nowrap dark:text-white' : 'self-center text-2xl font-semibold whitespace-nowra text-gray-900'">eDirect</span>
       </a>
-      <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-        <button type="button"
-          class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-          id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
-          data-dropdown-placement="bottom">
-          <span class="sr-only">Open user menu</span>
-          <img class="w-8 h-8 rounded-full" src="" alt="user photo">
+
+      <div class="flex items-center">
+        <button class="flex items-center mr-10" @click="toggleDarkMode">
+          <span v-if="!darkMode" class="material-symbols-outlined text-slate-800  dark:text-white mr-10">dark_mode</span>
+          <span v-else class="material-symbols-outlined  text-slate-800  dark:text-white mr-10">light_mode</span>
         </button>
-        <!-- Dropdown menu -->
-        <div
-          class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-          id="user-dropdown">
-          <div class="px-4 py-3">
-            <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-            <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+        <div v-if="route.name !== 'login' && route.name !== 'signup'"
+          class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button type="button"
+            class="flex items-center text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+            id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
+            data-dropdown-placement="bottom">
+            <span class="sr-only">Open user menu</span>
+            <span class="material-symbols-outlined text-3l ">
+              person
+            </span>
+          </button>
+
+          <!-- Dropdown menu -->
+          <div
+            class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+            id="user-dropdown">
+            <div class="px-4 py-3">
+              <span class="block text-sm text-gray-900 dark:text-white">{{ user?.username }}</span>
+              <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">{{ user?.email }}</span>
+            </div>
+            <ul class="py-2" aria-labelledby="user-menu-button">
+              <li>
+                <div class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+                  <button @click="handleSignOut"
+                    class="text-sm text-gray-700 dark:text-gray-200 dark:hover:text-white w-full text-left">
+                    Sign out
+                  </button>
+                </div>
+              </li>
+            </ul>
           </div>
-          <ul class="py-2" aria-labelledby="user-menu-button">
-            <li>
-              <a href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
-            </li>
-            <li>
-              <a href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
-            </li>
-            <li>
-              <a href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
-            </li>
-            <li>
-              <a href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
-                out</a>
-            </li>
-          </ul>
         </div>
-        <button data-collapse-toggle="navbar-user" type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-user" aria-expanded="false">
-          <span class="sr-only">Open main menu</span>
-          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15" />
-          </svg>
-        </button>
-      </div>
-      <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-        <ul
-          class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-          <li>
-            <a href="#"
-              class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-              aria-current="page">Home</a>
-          </li>
-          <li>
-            <a href="#"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-          </li>
-          <li>
-            <a href="#"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-          </li>
-          <li>
-            <a href="#"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>
-          </li>
-          <li>
-            <a href="#"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-          </li>
-        </ul>
+
       </div>
     </div>
   </nav>
 </template>
 
-<script setup lang="ts">
-import { onMounted } from 'vue'
-import { initFlowbite } from 'flowbite';
 
-onMounted(() => {
-  initFlowbite();
+<script setup lang = 'ts'>
+import { onMounted, ref, defineExpose, Ref } from 'vue'
+import { initFlowbite } from 'flowbite'
+import { useRoute, useRouter } from 'vue-router'
+import { signOut, onAuthStateChanged } from 'firebase/auth';
+import { auth, db } from '../../firebase/init.ts';
+import { doc, getDoc, DocumentData } from 'firebase/firestore';
+import Swal from 'sweetalert2';
+
+const route = useRoute();
+const router = useRouter();
+let darkMode = ref(getDarkMode());
+
+let body: HTMLBodyElement | null;
+
+let user: Ref<DocumentData | null> = ref(null);
+
+onAuthStateChanged(auth, (currentUser) => {
+  if (currentUser) {
+    const docRef = doc(db, 'users', currentUser.uid);
+    getDoc(docRef).then((docSnapshot) => {
+      if (docSnapshot.exists()) {
+        user.value = docSnapshot.data();
+      }
+    });
+  }
 });
+
+const handleSignOut = () => {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, sign out!',
+    width: '300px',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      signOut(auth)
+        .then(() => {
+          console.log('User signed out');
+          router.push({ name: 'login' });
+        })
+        .catch((error) => {
+          console.log('Error signing out: ', error);
+        });
+    }
+  })
+};
+
+function getDarkMode() {
+  // Try to get the dark mode setting from localStorage
+  const storedDarkMode = localStorage.getItem('darkMode');
+
+  // If the setting exists, return it (converted to a boolean)
+  if (storedDarkMode !== null) {
+    return storedDarkMode === 'true';
+  }
+
+  // Otherwise, determine the setting based on the body class
+  const isDark = body && body.classList.contains('dark');
+  return isDark ?? false;
+}
+
+function toggleDarkMode() {
+  darkMode.value = !darkMode.value;
+
+  // Store the new setting in localStorage
+  localStorage.setItem('darkMode', darkMode.value.toString());
+
+  if (body) {
+    body.classList.toggle('dark', darkMode.value);
+  }
+}
+
+defineExpose({
+  toggleDarkMode,
+  handleSignOut,
+});
+// initialize components based on data attribute selectors
+onMounted(() => {
+  body = document.querySelector('body');
+  initFlowbite();
+  body?.classList.toggle('dark', darkMode.value);
+})
+
 </script>
